9d3ec39206f576fc93a08d8dffe008d0cf4c9942
Hardi <hardivirani2003@gmail.com>
2026-06-07 22:59:07 -0400

    Resquest Mapping done

--- src/main/java/com/star/SpringBootWeb1/HomeController.java
+++ src/main/java/com/star/SpringBootWeb1/HomeController.java
@@ -1,12 +1,15 @@
 package com.star.SpringBootWeb1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
+    @RequestMapping("/home")
     public String home() {
-        System.out.println("home method calld");
+        System.out.println("home method called");
         return "index.jsp";
     }
 }
