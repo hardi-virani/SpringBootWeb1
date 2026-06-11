eec413c9d4c633b3fd367977224a4507ac268e19
Hardi <hardivirani2003@gmail.com>
2026-06-11 19:04:51 -0400

    Request Param

--- src/main/java/com/star/SpringBootWeb1/HomeController.java
+++ src/main/java/com/star/SpringBootWeb1/HomeController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -17,11 +18,12 @@ public class HomeController {
     }
 
     @RequestMapping("/Add")
-    public String add(HttpServletRequest req, HttpSession session) {
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, HttpSession session) {
 
         //But we can still simplify this code..
-       int num1 = Integer.parseInt(req.getParameter("num1"));
-       int num2 = Integer.parseInt(req.getParameter("num2"));
+//       int num1 = Integer.parseInt(req.getParameter("num1"));
+//       int num2 = Integer.parseInt(req.getParameter("num2"));
+
        int result = num1 + num2;
 
        session.setAttribute("Answer", result);
