8584ad003292a86d4e74c5c71c193b8adafec973
Hardi <hardivirani2003@gmail.com>
2026-06-11 17:36:26 -0400

    Sending data to controller

--- src/main/java/com/star/SpringBootWeb1/HomeController.java
+++ src/main/java/com/star/SpringBootWeb1/HomeController.java
@@ -1,5 +1,7 @@
 package com.star.SpringBootWeb1;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,4 +14,18 @@ public class HomeController {
         System.out.println("home method called");
         return "index.jsp";
     }
+
+    @RequestMapping("/Add")
+    public String add(HttpServletRequest req) {
+
+       int num1 = Integer.parseInt(req.getParameter("num1"));
+       int num2 = Integer.parseInt(req.getParameter("num2"));
+       int result = num1 + num2;
+
+        System.out.println(result);
+//        System.out.println("in add");
+
+        return "result.jsp";
+    }
+
 }
